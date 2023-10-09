20d04ffbf56b44b6b27b05c611839db97b9f5e00
Alicia Aranda <loliaranda29@gmail.com>
2023-10-09 20:40:33 -0300

    codigo modificado

--- app.js
+++ app.js
@@ -1,61 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const uuidv4 = require('uuid').v4;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
+const tareas = [
+  {
+    id: '1',
+    completo: true,
+    descripcion: 'Hacer las tareas',
+  },
+  {
+    id: '2',
+    completo: false,
+    descripcion: 'Estudiar',
+  },
+];
 
-const tasks = [];
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/tareas', (req, res) => {
+  res.json(tareas);
 });
 
-app.get('/list', (req, res) => {
-  const taskList = tasks.map((task) => ({
-    id: task.id,
-    isCompleted: task.completed,
-    description: task.description,
-  }));
+const PORT = 3000;
 
-  res.json(taskList);
-});
-
-app.post('/add', (req, res) => {
-  const description = req.body.description;
-  if (description) {
-    const id = uuidv4();
-    tasks.push({ id, description, completed: false });
-    res.redirect('/');
-  } else {
-    res.status(400).send('Descripción de tarea no proporcionada.');
-  }
-});
-
-app.post('/complete', (req, res) => {
-  const taskId = req.body.taskId;
-  const task = tasks.find((t) => t.id === taskId);
-  if (task) {
-    task.completed = true;
-    res.redirect('/');
-  } else {
-    res.status(400).send('ID de tarea no válido.');
-  }
-});
-
-app.post('/delete', (req, res) => {
-  const taskId = req.body.taskId;
-  const taskIndex = tasks.findIndex((t) => t.id === taskId);
-  if (taskIndex !== -1) {
-    tasks.splice(taskIndex, 1);
-    res.redirect('/');
-  } else {
-    res.status(400).send('ID de tarea no válido.');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Aplicación de tareas escuchando en http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
 });
